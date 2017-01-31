94bb63da792ab0a94421c160c888bcf19ab34297
Kay Ehni <kay.ehnigmail.com>
2017-01-31 09:42:15 -0500

    added draggable UI functionality

--- projects/towers-of-hanoi/script.js
+++ projects/towers-of-hanoi/script.js
@@ -1,23 +1,40 @@
-var disc1 = 1;
-var disc2 = 2;
-var disc3 = 3;
+disc1 =
+{
+  id: $("#disc1"),
+  value: 1,
+  pole: pole1
+}
+disc2 =
+{
+  id: $("#disc2"),
+  value: 2,
+  pole: pole1
+}
+disc3 =
+{
+  id: $("#disc3"),
+  value: 3,
+  pole: pole1
+}
 //var disc4 = 4;
 //var disc5 = 5;
 
-var pole1 =
+pole1 =
 {
-discs : [disc3,disc2,disc1],
-loc : 1
+  discs : [disc3,disc2,disc1],
+  id : $("#pole1")
 }
-var pole2 =
+
+pole2 =
 {
 discs : [],
-loc : 2
+id : $("#pole2")
 }
-var pole3 =
+
+pole3 =
 {
 discs : [],
-loc : 3
+id : $("#pole3")
 }
 
 function emptyPole (pole){                        //evaluates if pole is empty
@@ -30,7 +47,7 @@ function emptyPole (pole){                        //evaluates if pole is empty
 }
 
 function win() {
-  if(pole3.discs[0]==3 && pole3.discs[1]== 2 && pole3.discs[2] == 1)
+  if(pole3.discs[0]==disc3 && pole3.discs[1]== disc2 && pole3.discs[2] == disc1)
   {
   alert("You win!");
   return true;
@@ -41,13 +58,13 @@ function win() {
 }
 
 function moveLegal (poleOrg, poleNew) {
-  if ((emptyPole(poleNew) || poleNew.discs[poleNew.discs.length-1] > poleOrg.discs[poleOrg.discs.length-1] == true) && (poleOrg.discs.length > 0))
+  if ((emptyPole(poleNew) || poleNew.discs[poleNew.discs.length-1].value > poleOrg.discs[poleOrg.discs.length-1].value == true) && (poleOrg.discs.length > 0))
   {
   return true;
   }
 else
   {
-  return poleOrg.discs[poleOrg.discs.length-1];
+  console.log("illegal")
   }
 }
 
@@ -55,6 +72,7 @@ function move (poleOrg, poleNew) {
   if (moveLegal (poleOrg,poleNew) == true)
   {
   poleNew.discs.push(poleOrg.discs.pop());
+  poleNew.discs[poleNew.discs.length-1].id.prependTo(poleNew.id);
   win();
   console.log("Pole 1: " + pole1.discs,"Pole 2: " + pole2.discs,"Pole 3: " + pole3.discs);
   }
@@ -62,4 +80,37 @@ function move (poleOrg, poleNew) {
   {
   alert("U can't do that!");
   }
+}
+
+$(".discs").click(function(){
+  var poleOrg;
+  if($(this).prev().length == 0)
+  {                                         //current pole of disc clicked
+    /*move($(this).parent(),pole2)*/
+    if($(this).parent().attr("id") == "pole1")
+    {
+      poleOrg = pole1;
+    }
+    if($(this).parent().attr("id") == "pole2")
+    {
+      poleOrg = pole2;
+    }
+    if($(this).parent().attr("id") == "pole3")
+    {
+      poleOrg = pole3;
+    }
+  move(poleOrg,pole2);
+  /*move(poleOrg,pole2);*/
   }
+  else {
+  alert("inavlid move!")
+  console.log($(this).length)
+  }
+})
+
+$(function() {
+   $(".discs").draggable({containment: $("body")});
+   $("#pole2").droppable();
+   $("#pole3").droppable();
+   $("#pole1").droppable();
+ });
